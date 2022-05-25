b9715d6e21c48d3d292d2b9e796eb9644971a85e
Abby Gilmore <abbyngilmore@eaton.com>
2022-05-25 15:13:43 -0400

    testing

--- test.ps1
+++ test.ps1
@@ -10,3 +10,10 @@ curl \
  https://api.github.com/repos/abbyngilmore/AbbyTesting/pulls \
  -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"abbyngilmore:Abby-Feature-Test","base":"main"}'
  #>
+
+ curl \
+  -X PATCH \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER \
+  -d '{"title":"new title","body":"updated body","state":"open","base":"master"}'
+  # testing 
