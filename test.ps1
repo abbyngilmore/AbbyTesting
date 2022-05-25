0a844bf3938c7f50f85ff377c94479c5df4a63bb
Abby Gilmore <abbyngilmore@eaton.com>
2022-05-25 10:22:33 -0400

    commiting a change

--- test.ps1
+++ test.ps1
@@ -3,3 +3,10 @@ write-host "Hello World"
 #adding another comment
 
 #adding changes again with a comment
+<#
+curl \
+-X POST \
+ -H "Accept: application/vnd.github.v3+json" \
+ https://api.github.com/repos/abbyngilmore/AbbyTesting/pulls \
+ -d '{"title":"Amazing new feature","body":"Please pull these awesome changes in!","head":"abbyngilmore:Abby-Feature-Test","base":"main"}'
+ #>
